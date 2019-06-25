0ac128c3b333570c29b32c9fba511bccdd962ee7
AlijtRijcken <33155205+AlijtRijcken@users.noreply.github.com>
2019-06-25 19:55:44 +0200

    Gaande 5.0

--- MyApplication.cs
+++ MyApplication.cs
@@ -8,11 +8,11 @@ namespace Template
 	class MyApplication
 	{
 		// member variables
-		public Surface screen;                  // background surface for printing etc.
-		Mesh Tpot, Tfloor, Tpot2, Tpot3, Tfloor1, Tfloor2;        // a mesh to draw using OpenGL
-		Stopwatch timer;                        // timer for measuring frame duration
-		Texture wood, wood1, metal, rust;                           // texture to use for rendering
-        SceneGraph sceneGraph;                  // SceneGraph used to render the scene 
+		public Surface screen;                                                              // background surface for printing etc.
+		Mesh Tpot, Tfloor, Tpot2, Tpot3, Tfloor1, Tfloor2, Tfloor3;                         // a mesh to draw using OpenGL
+		Stopwatch timer;                                                                    // timer for measuring frame duration
+		Texture wood, wood1, metal, rust, blue, yellow;                                      // texture to use for rendering
+        SceneGraph sceneGraph;                                                              // SceneGraph used to render the scene 
         const float PI = 3.1415926535f;
        
         // initialize
@@ -25,14 +25,17 @@ namespace Template
             wood1 = new Texture("../../assets/wood1.jpg");
             metal = new Texture("../../assets/metal.jpg");
             rust = new Texture("../../assets/rust.jpg");
+            blue = new Texture("../../assets/blue.jpg");
+            yellow = new Texture("../../assets/yellow.jpg");
 
             // load meshes
             Tpot = new Mesh( "../../assets/teapot.obj", new Vector3(0, 0, 0), new Vector3(0.5f, 0.5f, 0.5f), new Vector3(0, 0, 0), new List<Mesh>(), wood);
-            Tpot2 = new Mesh( "../../assets/teapot.obj", new Vector3(0, 0, 0), new Vector3(0.5f, 0.5f, 0.5f), new Vector3(5f, 0, 0), new List<Mesh>(), metal);
+            Tpot2 = new Mesh( "../../assets/teapot.obj", new Vector3(0, 0, 0), new Vector3(0.5f, 0.5f, 0.5f), new Vector3(5f, 0, 0), new List<Mesh>(), blue);
             Tpot3 = new Mesh( "../../assets/teapot.obj", new Vector3(0, 0, 0), new Vector3(0.5f, 0.5f, 0.5f), new Vector3(-5f, 6f, 0), new List<Mesh>(), wood1);
 			Tfloor = new Mesh( "../../assets/floor.obj", new Vector3(0, 0, 0), new Vector3(4.0f, 4.0f, 4.0f), new Vector3(0, 0, 0), new List<Mesh>(), rust);
 			Tfloor1 = new Mesh( "../../assets/floor.obj", new Vector3(1, 0, 0), new Vector3(4.0f, 4.0f, 4.0f), new Vector3(0, 0, -25f), new List<Mesh>(), rust);
-			Tfloor2 = new Mesh( "../../assets/floor.obj", new Vector3(0, 1, 0), new Vector3(4.0f, 4.0f, 4.0f), new Vector3(0, 0, -25f), new List<Mesh>(), rust);
+			Tfloor2 = new Mesh( "../../assets/floor.obj", new Vector3(0, 1, 0), new Vector3(4.0f, 4.0f, 4.0f), new Vector3(25f, 0, 0), new List<Mesh>(), metal);
+            Tfloor3 = new Mesh("../../assets/floor.obj", new Vector3(1, 1, 0), new Vector3(4.0f, 4.0f, 4.0f), new Vector3(0, -1f, 0), new List<Mesh>(), yellow);
 
             //Add created meshes to the hierarchy tree
             sceneGraph.addPrimaryChild(Tpot);
@@ -44,6 +47,7 @@ namespace Template
             Tpot.addChild(Tpot3);
             Tfloor.addChild(Tfloor1);
             Tfloor.addChild(Tfloor2);
+            Tfloor.addChild(Tfloor3);
 
 
 			// initialize stopwatch
@@ -78,6 +82,12 @@ namespace Template
 
             Tpot3.angle.Y += 0.001f * frameDuration;
             if (Tpot3.angle.Y > 2 * PI) Tpot3.angle.Y -= 2 * PI;
+
+            Tfloor.angle.Y += 0.001f * frameDuration;
+            if (Tfloor.angle.Y > 2 * PI) Tfloor.angle.Y -= 2 * PI;
+
+            Tfloor3.angle.X += 0.001f * frameDuration;
+            if (Tfloor3.angle.X > 2 * PI) Tfloor3.angle.X -= 2 * PI;
 
 
             // render scene
